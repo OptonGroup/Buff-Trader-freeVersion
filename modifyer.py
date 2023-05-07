af8fa24a491bd2142341a20f03f269f2f3ba1acb
Opton <52974908+OptonGroup@users.noreply.github.com>
2023-05-07 20:16:00 +0300

    Update modifyer.py

--- modifyer.py
+++ modifyer.py
@@ -7,7 +7,19 @@ data = req.json()
 items = dict()
 
 for name in data:
-	price = round(min(data[name]["last_24h"], data[name]["last_7d"], data[name]["last_30d"], data[name]["last_90d"])/0.14475,2)
+	price = 1000000000000
+	if data[name]["last_24h"] != 0:
+		price = min(price, data[name]["last_24h"])
+	if data[name]["last_7d"] != 0:
+		price = min(price, data[name]["last_7d"])
+	if data[name]["last_30d"] != 0:
+		price = min(price, data[name]["last_30d"])
+	if data[name]["last_90d"] != 0:
+		price = min(price, data[name]["last_90d"])
+		
+	if price == 1000000000000:
+		price = 0
+	price = round(price/0.14475, 2)
 	items[name] = price
   
 with open("json.json", "w", encoding="utf-8") as file:
