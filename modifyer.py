879e2be6544cf6c60fa2a178d25b75d70b92a9dd
Opton <52974908+OptonGroup@users.noreply.github.com>
2023-05-08 17:10:35 +0000

    modified:   json.json 	modified:   modifyer.py

--- modifyer.py
+++ modifyer.py
@@ -1,26 +1,42 @@
 import json
 import requests
 
-req = requests.get("https://prices.csgotrader.app/latest/steam.json")
-data = req.json()
-
 items = dict()
 
-for name in data:
-	price = 1000000000000
-	if data[name]["last_24h"] != 0:
-		price = min(price, data[name]["last_24h"])
-	if data[name]["last_7d"] != 0:
-		price = min(price, data[name]["last_7d"])
-	if data[name]["last_30d"] != 0:
-		price = min(price, data[name]["last_30d"])
-	if data[name]["last_90d"] != 0:
-		price = min(price, data[name]["last_90d"])
-		
-	if price == 1000000000000:
-		price = 0
-	price = round(price/0.14475, 2)
-	items[name] = price
+def get_prices_from_steam():
+	req = requests.get("https://prices.csgotrader.app/latest/steam.json")
+	data = req.json()
+
+	for name in data:
+		price = 1000000000000
+		if data[name]["last_24h"] != 0:
+			price = min(price, data[name]["last_24h"])
+		if data[name]["last_7d"] != 0:
+			price = min(price, data[name]["last_7d"])
+		if data[name]["last_30d"] != 0:
+			price = min(price, data[name]["last_30d"])
+		if data[name]["last_90d"] != 0:
+			price = min(price, data[name]["last_90d"])
+			
+		if price == 1000000000000:
+			price = 0
+		price = round(price/0.14475, 2)
+		items[name] = price
+
+def get_prices_from_buff163():
+	req = requests.get("https://prices.csgotrader.app/latest/buff163.json")
+	data = req.json()
+
+	for name in data:
+		try:
+			price = data[name]["starting_at"]["price"]
+			price = round(price/0.14475, 2)
+		except:
+			price = 0
+
+		items[name] = price
+
+get_prices_from_buff163()
   
 with open("json.json", "w", encoding="utf-8") as file:
 	json.dump(items, file, ensure_ascii=False, indent=4)
