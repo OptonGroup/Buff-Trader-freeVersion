bd76e0e02eea7c0dff078f22543ccc5c57accb4d
Opton <52974908+OptonGroup@users.noreply.github.com>
2023-05-15 15:57:46 +0000

    message: updated prices 	modified:   json.json 	modified:   modifyer.py

--- modifyer.py
+++ modifyer.py
@@ -4,39 +4,39 @@ import requests
 items = dict()
 
 def get_prices_from_steam():
-	req = requests.get("https://prices.csgotrader.app/latest/steam.json")
-	data = req.json()
+    req = requests.get("https://prices.csgotrader.app/latest/steam.json")
+    data = req.json()
 
-	for name in data:
-		price = 1000000000000
-		if data[name]["last_24h"] != 0:
-			price = min(price, data[name]["last_24h"])
-		if data[name]["last_7d"] != 0:
-			price = min(price, data[name]["last_7d"])
-		if data[name]["last_30d"] != 0:
-			price = min(price, data[name]["last_30d"])
-		if data[name]["last_90d"] != 0:
-			price = min(price, data[name]["last_90d"])
-			
-		if price == 1000000000000:
-			price = 0
-		price = round(price/0.14475, 2)
-		items[name] = price
+    for name in data:
+        price = 1000000000000
+        if data[name]["last_24h"] != 0:
+            price = min(price, data[name]["last_24h"])
+        if data[name]["last_7d"] != 0:
+            price = min(price, data[name]["last_7d"])
+        if data[name]["last_30d"] != 0:
+            price = min(price, data[name]["last_30d"])
+        if data[name]["last_90d"] != 0:
+            price = min(price, data[name]["last_90d"])
+            
+        if price == 1000000000000:
+            price = 0
+        price = round(price/0.14475, 2)
+        items[name] = price
 
 def get_prices_from_buff163():
-	req = requests.get("https://prices.csgotrader.app/latest/buff163.json")
-	data = req.json()
+    req = requests.get("https://prices.csgotrader.app/latest/buff163.json")
+    data = req.json()
 
-	for name in data:
-		try:
-			price = data[name]["highest_order"]["price"]
-			price = round(price/0.1447, 2)
-		except:
-			price = 0
+    for name in data:
+        try:
+            price = data[name]["highest_order"]["price"]
+            price = round(price/0.1447, 2)
+        except:
+            price = 0
 
-		items[name] = price
+        items[name] = price
 
 get_prices_from_buff163()
   
 with open("json.json", "w", encoding="utf-8") as file:
-	json.dump(items, file, ensure_ascii=False, indent=4)
+    json.dump(items, file, ensure_ascii=False, indent=4)
