2c87051421da40d49251488d39d86a4e366b2270
Opton <52974908+OptonGroup@users.noreply.github.com>
2023-05-21 04:17:14 +0000

    modified:   json.json 	modified:   modifyer.py

--- modifyer.py
+++ modifyer.py
@@ -2,7 +2,7 @@ import json
 import requests
 
 items = dict()
-from_usd_to_rbm = 0.143472
+from_usd_to_rbm = 0.142248
 
 def get_prices_from_steam():
     req = requests.get("https://prices.csgotrader.app/latest/steam.json")
