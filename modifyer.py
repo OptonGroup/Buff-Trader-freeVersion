3fc270f84e261e547c75a1296bab03868e18f98e
Opton <52974908+OptonGroup@users.noreply.github.com>
2024-04-29 07:58:38 +0000

    modified:   json.json 	modified:   modifyer.py

--- modifyer.py
+++ modifyer.py
@@ -4,26 +4,6 @@ import requests
 items = dict()
 from_usd_to_rbm = 7.24
 
-def get_prices_from_steam():
-    req = requests.get("https://prices.csgotrader.app/latest/steam.json")
-    data = req.json()
-
-    for name in data:
-        price = 1000000000000
-        if data[name]["last_24h"] != 0:
-            price = min(price, data[name]["last_24h"])
-        if data[name]["last_7d"] != 0:
-            price = min(price, data[name]["last_7d"])
-        if data[name]["last_30d"] != 0:
-            price = min(price, data[name]["last_30d"])
-        if data[name]["last_90d"] != 0:
-            price = min(price, data[name]["last_90d"])
-            
-        if price == 1000000000000:
-            price = 0
-        price = round(price/from_usd_to_rbm, 2)
-        items[name] = price
-
 def get_prices_from_buff163():
     req = requests.get("https://prices.csgotrader.app/latest/buff163.json")
     data = req.json()
